6efb9a999b3815e3268701096f15649837b66069
KIMJISOO <fiapie@kakao.com>
2021-06-03 10:22:50 +0900

    안네

--- teamPractice/src/main/java/member/writeService.java
+++ teamPractice/src/main/java/member/writeService.java
@@ -2,6 +2,6 @@ package member;
 
 public class writeService {
 	public static void main(String[] args) {
-		System.out.println("안녕 안녕 여러분 안녕 녕안안");
+		System.out.println("안녕 안녕 여러분 안녕 녕안안안안네네");
 	}
 }
